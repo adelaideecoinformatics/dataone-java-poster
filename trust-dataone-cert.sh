911d21c59a20451c42d8c0d4660fe9e18c5995b8
Tom Saleeba <tom.saleeba@gmail.com>
2015-01-14 16:37:46 +1030

    EIR-1161 added a bash script for trusting the dataone node

---
+++ trust-dataone-cert.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Downloads the public certificate from the AEKOS DataONE member node
+# and adds it to the trusted certificates keystore. This is required
+# to be able to connect to the server to POST data to it.
+
+NODE_ADDRESS=130.220.209.107
+CERTIFICATE_PATH=/tmp/aekos-dataone-pub.cert
+SERVER_NAME=aekos-dataone
+ARG_JAVA_HOME=$1
+if [ -z "$ARG_JAVA_HOME" ]; then
+  echo "Error! You need to tell us the path to the JRE 'bin' directory."
+  echo " This directory needs to contain the 'keytool' binary and ../lib/security/cacerts will exist"
+  echo "  usage: $0 <path/to/java/bin>"
+  echo "  e.g: $0 /usr/lib/jvm/java-7-oracle/jre/bin"
+  exit 1
+fi
+
+SECURITY_DIR=$ARG_JAVA_HOME/../lib/security
+if [ ! -d "$SECURITY_DIR" ]; then
+  echo "Error! The passed java bin directory doesn't have $SECURITY_DIR as a valid relative path"
+  echo "  Have you checked inside the jre/ directory in the java install?"
+  exit 1
+fi
+
+echo "Downloading certificate to $CERTIFICATE_PATH"
+openssl s_client -connect $NODE_ADDRESS:443 < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > $CERTIFICATE_PATH
+echo "Adding trusted certificate..."
+echo "  If you get prompted for a password, the default is: changeit"
+$ARG_JAVA_HOME/keytool -import -alias $SERVER_NAME -keystore $SECURITY_DIR/cacerts -file $CERTIFICATE_PATH
+echo "Done :D"
+if [ "$(whoami)" != "root" ]; then
+  echo "WARNING! You didn't run as sudo/root so if that failed, try again as a superuser"
+fi
